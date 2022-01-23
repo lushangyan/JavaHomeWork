8835d50d70bdf85565e8af1b74e6eddea5896f79
lushangyan <765277920@qq.com>
2022-01-24 03:14:06 +0800

    第三周作业：2.（选做）使用 Netty 实现后端 HTTP 访问（代替上一步骤）。

---
+++ week03/nio02/src/main/java/io/github/kimmking/gateway/outbound/netty4/NettyHttpClientOutboundHandler.java
@@ -0,0 +1,32 @@
+package io.github.kimmking.gateway.outbound.netty4;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+
+import java.net.SocketAddress;
+
+public class NettyHttpClientOutboundHandler  extends ChannelOutboundHandlerAdapter {
+
+    public NettyHttpClientOutboundHandler() {
+        super();
+    }
+
+
+    @Override
+    public void connect(ChannelHandlerContext ctx, SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) throws Exception {
+        super.connect(ctx, remoteAddress, localAddress, promise);
+    }
+
+    @Override
+    public void read(ChannelHandlerContext ctx) throws Exception {
+        super.read(ctx);
+    }
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+        super.write(ctx, msg, promise);
+    }
+
+
+}
